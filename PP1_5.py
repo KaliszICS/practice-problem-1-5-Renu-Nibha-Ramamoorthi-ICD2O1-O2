c6eb6333abb9fab1e4cbebcceb7acba18da39b4f
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-11-06 13:46:31 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,35 +1,33 @@
 
 def q1():
   #Write Assignment code here
-  num = input("Input an integer: ") 
-  num = int(num) 
-  num += 3 
-  print(num) 
+  user_input = input("Input an integer: ") 
+  result = int(user_input) + 3 
+  print("The result is:", result) 
 def q2():
   #Write Assignment code here
-  num = input("Input a number: ") 
-  num = float(num + "4") 
-  num += 2 
-  print(num) 
+  user_input = input("Input a number: ") 
+  concatenated_input = user_input + '4'
+  result = float(concatenated_input) + 2
+  print("The result is:", result) 
 def q3():
   #Write Assignment code here
   radius = input("Input a radius: ") 
   radius = float(radius) 
-  area = 3.14 * radius * radius 
+  pi = 3.14 
+  area = pi * (radius ** 2) 
   print("The area of the circles is:", area) 
 def q4():
   #Write Assignment code here
-  num = input("Input a number: ") 
-  num = float(num) 
-  num * = 12 
-  num = math.floor(num)
-  print(num) 
+  user_input = input("Input a number: ") 
+  number = float(user_input)  
+  final_value = math.floor(number * 12)
+  print("The final value is:", final_value) 
 def q5():
   #Write Assignment code here
-  num = input("Input an integer: ")
-  num = int(num)
-  num += 5 
-  print("Your number + 5 is {}".format(num)) 
+  user_input = input("Input an integer: ")
+  number = int(user_input) + 5  
+  print(f"Your number + 5 is {number}") 
               
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
